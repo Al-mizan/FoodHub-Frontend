14e9e7278f3efc2e89625bfa51f6e43baef37195
Al-mizan <mdalmizanakon@gmail.com>
2026-02-21 14:53:57 +0600

    fix: env.ts

--- src/env.ts
+++ src/env.ts
@@ -3,23 +3,17 @@ import * as z from "zod";
 
 export const env = createEnv({
     server: {
-        BACKEND_API: z.url(),
-        FRONTEND_API: z.url(),
-        API_URL: z.url(),
+        BACKEND_API: z.url().optional(),
     },
     client: {
         NEXT_PUBLIC_BACKEND_API: z.url(),
         NEXT_PUBLIC_API_URL: z.url(),
         NEXT_PUBLIC_APP_URL: z.url(),
-        // NEXT_PUBLIC_NODE_ENV: z.enum(["development", "production", "test"]), it has buildin feature
     },
     runtimeEnv: {
         BACKEND_API: process.env.BACKEND_API,
-        FRONTEND_API: process.env.FRONTEND_API,
-        API_URL: process.env.API_URL,
         NEXT_PUBLIC_BACKEND_API: process.env.NEXT_PUBLIC_BACKEND_API,
         NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
         NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
-        // NEXT_PUBLIC_NODE_ENV: process.env.NEXT_PUBLIC_NODE_ENV,
     },
 });
